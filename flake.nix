b3b4bd2e0f36a65279b3dc3dc143ba469377827d
Thomas Kirschner <thomaskirschner85@yahoo.de>
2026-05-31 03:13:47 +0200

    Properly wrapped all functions

--- flake.nix
+++ flake.nix
@@ -3,13 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, nixpkgs-unstable, ... }:
     let
       system = "x86_64-linux";
+
       pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          segger-jlink.acceptLicense = true;
+        };
+      };
+
+      pkgs-unstable = import nixpkgs-unstable {
         inherit system;
         config = {
           allowUnfree = true;
@@ -19,40 +29,48 @@
 
       toolchain = "/home/thomas-workstation/ncs/toolchains/911f4c5c26";
       ncs = "/home/thomas-workstation/ncs/v3.3.0";
+
+      openocdWrapped = pkgs.writeShellScriptBin "openocd" ''
+        export LD_LIBRARY_PATH="${pkgs-unstable.systemd}/lib:''${LD_LIBRARY_PATH:-}"
+        exec ${pkgs-unstable.openocd}/bin/openocd "$@"
+      '';
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         name = "le-audio-receiver";
 
-        buildInputs = with pkgs; [
-          nrfutil
-          pyocd
-        ];
+        buildInputs =
+          with pkgs;
+          [
+            nrfutil
+            pyocd
+          ]
+          ++ [
+            openocdWrapped
+            pkgs-unstable.systemd
+          ];
 
         shellHook = ''
-          # nRF Connect SDK v3.3.0 toolchain environment.
           TC=${toolchain}
           NCS=${ncs}
-
-          export PATH="$TC/usr/bin:$TC/usr/local/bin:$TC/opt/bin:$TC/opt/zephyr-sdk/arm-zephyr-eabi/bin:$TC/opt/zephyr-sdk/riscv64-zephyr-elf/bin:$TC/opt/nanopb/generator-bin:$TC/nrfutil/bin:$PATH"
-
-          export LD_LIBRARY_PATH="$TC/usr/lib:$TC/usr/lib/x86_64-linux-gnu:$TC/usr/local/lib:$LD_LIBRARY_PATH"
 
           export ZEPHYR_BASE="$NCS/zephyr"
           export ZEPHYR_SDK_INSTALL_DIR="$TC"
 
-          # Do not leak Nordic Python into unrelated Python tools.
+          # NCS / Zephyr toolchain binaries.
+          export PATH="$TC/usr/bin:$TC/usr/local/bin:$TC/opt/bin:$TC/opt/zephyr-sdk/arm-zephyr-eabi/bin:$TC/opt/zephyr-sdk/riscv64-zephyr-elf/bin:$TC/opt/nanopb/generator-bin:$TC/nrfutil/bin:$PATH"
+
+          # Do not globally poison Python or dynamic linker state.
           unset PYTHONHOME
           unset PYTHONPATH
           unset _PYTHON_HOST_PLATFORM
           unset _PYTHON_SYSCONFIGDATA_NAME
+          unset LD_LIBRARY_PATH
 
-          # Run west with the Nordic toolchain Python explicitly.
           west-ncs() {
             "$TC/usr/local/bin/python3" -m west "$@"
           }
 
-          # Optional convenience aliases.
           alias west='west-ncs'
         '';
       };
